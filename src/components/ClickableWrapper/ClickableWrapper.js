c6a8d62b17b53df9f4be240202474223c793a34f
Nick C <niemal@users.noreply.github.com>
2023-03-30 17:27:14 +0300

    Proper outlining behaviour

--- src/components/ClickableWrapper/ClickableWrapper.js
+++ src/components/ClickableWrapper/ClickableWrapper.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ClickableWrapper = ({ children, onClick, ...props }) => {
+  const [isKeyboardFocused, setIsKeyboardFocused] = useState(false);
   const handleKeyDown = (event) => {
     if (event.key === " " || event.key === "Enter") {
       event.preventDefault();
@@ -9,15 +10,62 @@ const ClickableWrapper = ({ children, onClick, ...props }) => {
         onClick(event);
       }
     }
+
+    if (firstChild.props.onKeyDown) {
+      firstChild.props.onKeyDown(event);
+    }
+
+    if (event.key === "Tab") {
+      setIsKeyboardFocused(true);
+    }
   };
+
+  const handleMouseDown = (e) => {
+    if (firstChild.props.onMouseDown) {
+      firstChild.props.onMouseDown(e);
+    }
+
+    setIsKeyboardFocused(false);
+  };
+
+  const handleBlur = (e) => {
+    if (firstChild.props.onBlur) {
+      firstChild.props.onBlur(e);
+    }
+
+    setIsKeyboardFocused(false);
+  };
+
+  const handleKeyUp = (e) => {
+    if (firstChild.props.onKeyUp) {
+      firstChild.props.onKeyUp(e);
+    }
+
+    if (e.key === "Tab") {
+      setIsKeyboardFocused(true);
+    }
+  };
+
+  let { style, ...myProps } = props;
+  if (isKeyboardFocused) {
+    if (!style) {
+      style = {};
+    }
+    style.outline = "none !important";
+  }
 
   const firstChild = React.Children.toArray(children)[0];
   const enhancedChild = React.cloneElement(firstChild, {
     tabIndex: "0",
     role: "button",
     onKeyDown: handleKeyDown,
+    onKeyUp: handleKeyUp,
+    onMouseDown: handleMouseDown,
+    onBlur: handleBlur,
     onClick,
-    ...props,
+    isKeyboardFocused,
+    style,
+    ...myProps,
     ...firstChild.props,
   });
 
